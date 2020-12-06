219912383358c5f7ca5156c2623be2efd340a1cc
jakub_izbicki <jakub.izbicki1@gmail.com>
2020-12-06 15:28:47 +0100

    Reinstall tailwindcss with postcss 7 compatibility, rename Scene1

---
+++ src/views/Scene1.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-lg">
+    Hello there
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Home extends Vue {}
+</script>
