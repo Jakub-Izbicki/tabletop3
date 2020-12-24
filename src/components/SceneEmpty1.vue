a433417392f38e04f54c3c3aa17ebcf09176d8bd
Jakub Izbicki <jakub.izbicki1@gmail.com>
2020-12-24 14:40:23 +0100

    Disable css3s rendered, enable gl renderer

--- src/components/SceneEmpty1.vue
+++ src/components/SceneEmpty1.vue
@@ -22,9 +22,9 @@ import {
 } from "three/examples/jsm/renderers/CSS3DRenderer";
 
 export default class SceneEmpty1 extends Vue {
-  // private renderer!: WebGLRenderer;
+  private renderer!: WebGLRenderer;
 
-  private css3dRenderer!: CSS3DRenderer;
+  // private css3dRenderer!: CSS3DRenderer;
 
   private camera!: Camera;
 
@@ -45,37 +45,37 @@ export default class SceneEmpty1 extends Vue {
     const height = sceneWindow.offsetHeight;
     const scene = new THREE.Scene();
 
-    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
     camera.position.x = -90;
     camera.position.y = 200;
     camera.position.z = 105;
 
-    // const renderer: WebGLRenderer = new THREE.WebGLRenderer({
-    //   alpha: true,
-    //   antialias: true
-    // });
-    // renderer.setSize(width, height);
-    // renderer.domElement.style.position = "absolute";
-    // renderer.domElement.style.top = "0";
-    // renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    // renderer.shadowMap.enabled = true;
+    const renderer: WebGLRenderer = new THREE.WebGLRenderer({
+      alpha: true,
+      antialias: true
+    });
+    renderer.setSize(width, height);
+    renderer.domElement.style.position = "absolute";
+    renderer.domElement.style.top = "0";
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.shadowMap.enabled = true;
 
-    const css3dRenderer: CSS3DRenderer = new CSS3DRenderer();
-    css3dRenderer.setSize(width, height);
-    css3dRenderer.domElement.style.position = "absolute";
-    css3dRenderer.domElement.style.top = "0";
+    // const css3dRenderer: CSS3DRenderer = new CSS3DRenderer();
+    // css3dRenderer.setSize(width, height);
+    // css3dRenderer.domElement.style.position = "absolute";
+    // css3dRenderer.domElement.style.top = "0";
 
-    // this.renderer = renderer;
-    this.css3dRenderer = css3dRenderer;
+    this.renderer = renderer;
+    // this.css3dRenderer = css3dRenderer;
     this.camera = camera;
     this.scene = scene;
     this.css3dScene = new THREE.Scene();
 
-    // sceneWindow.appendChild(renderer.domElement);
-    sceneWindow.appendChild(css3dRenderer.domElement);
+    sceneWindow.appendChild(renderer.domElement);
+    // sceneWindow.appendChild(css3dRenderer.domElement);
 
-    // const controls = new OrbitControls(camera, this.renderer.domElement);
-    const controls = new OrbitControls(camera, this.css3dRenderer.domElement);
+    const controls = new OrbitControls(camera, this.renderer.domElement);
+    // const controls = new OrbitControls(camera, this.css3dRenderer.domElement);
     controls.screenSpacePanning = false;
     controls.maxDistance = 500;
     controls.minDistance = 10;
@@ -84,26 +84,30 @@ export default class SceneEmpty1 extends Vue {
 
     scene.add(new THREE.AmbientLight(0x666666));
 
-    // const light = new THREE.DirectionalLight(0xdedede, 1);
-    // light.position.set(0, 200, -100);
-    // light.shadow.bias = -0.001;
-    // light.position.set(0, 1000, 0);
-    // light.position.multiplyScalar(1.3);
-    // light.castShadow = true;
-    // light.shadow.mapSize.width = 1024;
-    // light.shadow.mapSize.height = 1024;
-    // const d = 100;
-    // light.shadow.camera.left = -d;
-    // light.shadow.camera.right = d;
-    // light.shadow.camera.top = d;
-    // light.shadow.camera.bottom = -d;
-    // light.shadow.camera.far = 1000;
-    // scene.add(light);
+    const light = new THREE.DirectionalLight(0xdedede, 1);
+    light.position.set(0, 200, 0);
+    light.shadow.bias = -0.001;
+    light.position.multiplyScalar(1.3);
+    light.castShadow = true;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.left = -1000;
+    light.shadow.camera.right = 1000;
+    light.shadow.camera.top = 1000;
+    light.shadow.camera.bottom = -1000;
+    light.shadow.camera.far = 1000;
+    scene.add(light);
+
+    const lightHelper = new THREE.DirectionalLightHelper(light);
+    scene.add(lightHelper);
+
+    const cameraHelper = new THREE.CameraHelper(light.shadow.camera);
+    scene.add(cameraHelper);
 
     const groundMesh = new THREE.Mesh(
       new THREE.PlaneBufferGeometry(20000, 20000),
       new THREE.MeshStandardMaterial({
-        color: 0xffffff,
+        color: 0xffaaaa,
         flatShading: true
       })
     );
@@ -121,11 +125,11 @@ export default class SceneEmpty1 extends Vue {
     const linesGeometry = new THREE.BufferGeometry().setFromPoints(points);
     scene.add(new THREE.Line(linesGeometry, lineBasicMaterial));
 
-    const side = 30;
+    const side = 10;
 
     [...new Array(side).keys()].forEach(i => {
       [...new Array(side).keys()].forEach(j => {
-        this.createTextPlane((i * 110) - ((side * 110) / 2), 0, (j * 110) - ((side * 110) / 2));
+        this.createTextPlane((i * 110) - ((side * 110) / 2), 10, (j * 110) - ((side * 110) / 2));
       }
       );
     }
@@ -135,46 +139,46 @@ export default class SceneEmpty1 extends Vue {
   animate() {
     requestAnimationFrame(this.animate);
     // this.controls.update();
-    // this.renderer.render(this.scene, this.camera);
-    this.css3dRenderer.render(this.css3dScene, this.camera);
+    this.renderer.render(this.scene, this.camera);
+    // this.css3dRenderer.render(this.css3dScene, this.camera);
   }
 
   createTextPlane(x: number, y: number, z: number) {
-    const element = document.createElement("div");
-    element.style.width = "100px";
-    element.style.height = "100px";
-    element.style.background = new THREE.Color(
-      Math.random() * 0.21568627451 + 0.462745098039,
-      Math.random() * 0.21568627451 + 0.462745098039,
-      Math.random() * 0.21568627451 + 0.462745098039
-    ).getStyle();
-    element.textContent = "Hello world";
+    // const element = document.createElement("div");
+    // element.style.width = "100px";
+    // element.style.height = "100px";
+    // element.style.background = new THREE.Color(
+    //   Math.random() * 0.21568627451 + 0.462745098039,
+    //   Math.random() * 0.21568627451 + 0.462745098039,
+    //   Math.random() * 0.21568627451 + 0.462745098039
+    // ).getStyle();
+    // element.textContent = "Hello world";
     // element.setAttribute("contenteditable", "");
-    const textElement = new CSS3DObject(element);
-    textElement.position.x = x;
-    textElement.position.y = y;
-    textElement.position.z = z;
-    textElement.rotation.x = -Math.PI / 2;
+    // const textElement = new CSS3DObject(element);
+    // textElement.position.x = x;
+    // textElement.position.y = y;
+    // textElement.position.z = z;
+    // textElement.rotation.x = -Math.PI / 2;
 
-    // const geometry = new THREE.PlaneBufferGeometry(100, 100);
-    // const material = new THREE.MeshStandardMaterial({
-    //   // wireframe: true,
-    //   flatShading: true,
-    //   side: THREE.DoubleSide
-    // });
-    // material.color.set("black");
+    const geometry = new THREE.PlaneBufferGeometry(100, 100);
+    const material = new THREE.MeshStandardMaterial({
+      // wireframe: true,
+      flatShading: true,
+      side: THREE.DoubleSide
+    });
+    material.color.set("grey");
     // material.opacity = 0;
     // material.blending = NoBlending;
-    // const plane = new THREE.Mesh(geometry, material);
-    // plane.castShadow = true;
-    // plane.receiveShadow = true;
-    // plane.position.x = x;
-    // plane.position.y = y;
-    // plane.position.z = z;
-    // plane.rotation.x = -Math.PI / 2;
+    const plane = new THREE.Mesh(geometry, material);
+    plane.castShadow = true;
+    plane.receiveShadow = true;
+    plane.position.x = x;
+    plane.position.y = y;
+    plane.position.z = z;
+    plane.rotation.x = -Math.PI / 2;
 
-    this.css3dScene.add(textElement);
-    // this.scene.add(plane);
+    // this.css3dScene.add(textElement);
+    this.scene.add(plane);
   }
 }
 </script>
